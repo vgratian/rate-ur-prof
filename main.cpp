63b117be5bce1c12513ac398e1a22f935530b1a2
imandes0 <vgratian@utopianlab.am>
2017-02-06 21:22:40 +0400

    finalized the C interface

--- main.cpp
+++ main.cpp
@@ -1,27 +1,50 @@
+// C++ libraries
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
-#include "professor.cpp"
+
+// C libraries
+#include <stdio.h>
+#include <string.h>
+
+// Components of software
 #include "student.cpp"
-#include "course.cpp"
-#include "rating.cpp"
+#include "professor.cpp"
 #include "session.cpp"
 
+/*
+This is a simple class in C that will allow to use the code writtin in C++
+as a C library. Isn't it amazing how a few hundreds of lines of code are
+accesible with just one simple method? TBC: this isn't a rhetorical question,
+I really if this creative solution will not cause problems in the future :)
+*/
+
+class C_mediator {
+private:
+  Session* session;
+public:
+  C_mediator() {
+    session = new Session;
+  }
+  const char* tunnel(const char* query) {
+    const char* response;
+    response = session->welcome(query);
+    return response;
+  }
+};
+
 extern "C" {
-  Session* Session_new() {
-    return new Session();
+  C_mediator* tunnel_init() {
+    return new C_mediator();
+  }
+  const char* tunnel_start(C_mediator* new_tunnel, const char* query) {
+    const char* msg = new_tunnel->tunnel(query);
+    return msg;
   }
 }
 
-/* Compile this as a shared library:
-g++ -c -fPIC main.cpp -o main.o
-g++ -shared -Wl,-soname,libfoo.so -o libfoo.so  main.o
-*/
-
 int main() {
-
-  Session hello;
 
   return 0;
 }

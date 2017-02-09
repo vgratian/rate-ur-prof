66266874b036b93de052f1bd0a089370a9b18e6a
imandes0 <vgratian@utopianlab.am>
2017-02-09 16:43:03 +0400

    fixed minor issue in flask causing error

--- setup.py
+++ setup.py
@@ -1,38 +1,41 @@
+
 """
 This is a preliminary Python interface (now working console-based)
 that needs to be linked to a web-framework (flask or django)
 """
+
 from flask import Flask, session, redirect, url_for, escape, request, render_template
 from ctypes import cdll, c_char_p, create_string_buffer
 clib = cdll.LoadLibrary('./libfoo.so')
 
 class Session(object):
+
+    query = ''
+
     def __init__(self):
         self.obj = clib.tunnel_init()
 
-    def talk(self, user_input):
+    def talk(self):
+        user_input = self.query
         clib.tunnel_start.restype = c_char_p
         arg = create_string_buffer(user_input.encode('utf-8'))
         result = clib.tunnel_start(self.obj, arg)
         return str(result)
 
 hello = Session()
-query = ""
-
 app = Flask(__name__)
 hotword = ''
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if  request.method == 'POST':
-        session['query'] = request.form['query']
+        hello.query = request.form['query']
         return redirect(url_for('home'))
     else:
-        query = session['query']
-        text = hello.talk(query)
-        return text + render_template('index.html') + "\nQUERY: " + query
+        text = hello.talk()
+        return text + render_template('index.html') + "\nQUERY: " + hello.query
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000)
+    app.run(host='127.0.0.1',port=8888)
